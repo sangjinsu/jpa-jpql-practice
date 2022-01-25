677cb540b2e9ce59fd590e699b94a61d79bab2ea
jinsuSang <climbplant39@kakao.com>
2022-01-25 22:57:47 +0900

    update jpql practice

---
+++ src/main/java/hellojpq/jpql/JpaMain.java
@@ -0,0 +1,40 @@
+package hellojpq.jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query1 = em.createQuery("select m.username from Member m", String.class);
+            Query query2 = em.createQuery("select m.username, m.age from Member m");
+
+            List<Member> resultList = query.getResultList();
+
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
